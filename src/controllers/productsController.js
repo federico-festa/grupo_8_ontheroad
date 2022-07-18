5a8e5466a15b5cc57165064b6f71f3f9eb3a7269
federico_festa <federico.sfesta@gmail.com>
2022-07-18 20:05:02 -0300

    upload admin.ejs

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -6,6 +6,9 @@ const productsController = {
     jujuy: (req, res) => {
         res.render('jujuy');    
     },
+    admin: (req,res) => {
+        res.render('admin');
+    }
 };
 
 module.exports = productsController;
