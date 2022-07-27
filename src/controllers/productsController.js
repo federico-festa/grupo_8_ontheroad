722da255f8ed4ab9941de8fb8ee1fb7c47b14061
federico_festa <federico.sfesta@gmail.com>
2022-07-27 20:30:42 -0300

    sprint 4 crud

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,13 +1,34 @@
 const path = require('path');
 const productsController = {
+    products: (req,res) => {
+        res.render('products');
+    },
+    detail: (req,res) => {
+        res.render('deatail');
+    },
+    create: (req,res) => {
+        res.render('product-create-form');
+    },
+    store: (req,res) => {
+        //Logica para crear producto
+        res.redirect('/');
+    },
+    edit: (req,res) => {
+        res.render('product-edit-form');
+    },
+    update: (req,res) => {
+        //Logica para editar producto
+        res.redirect('/');
+    },
+    destroy: (req,res) => {
+        //Logica para eliminar producto
+        res.redirect('/');
+    },
     productCart: (req, res) => {
         res.render('cart');    
     },
     jujuy: (req, res) => {
         res.render('jujuy');    
-    },
-    admin: (req,res) => {
-        res.render('admin');
     }
 };
 
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
 
-router.get('/admin', productsController.admin);
+router.get('/products', productsController.admin);
 router.get('/cart', productsController.productCart);
 router.get('/jujuy', productsController.jujuy);
 
