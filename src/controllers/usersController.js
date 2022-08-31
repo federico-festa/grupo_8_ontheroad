5375bc32331ce3402e9ac26faaaac47ad9305c59
federico_festa <federico.sfesta@gmail.com>
2022-08-31 20:28:18 -0300

    sequelize

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -9,29 +9,70 @@ const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 const userController = {
     register: (req, res) => {
         res.render('register');    
-    },
-    login: (req, res) => {
-        res.render('login');    
-    },
+    },    
     create: (req,res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
              res.render('register', { errors: errors.mapped() });
-           };
-        let newUser = req.body;
-        newUser.id = (users.length + 1);
-        newUser.image = req.file.filename;
-        newUser.password = bcrypt.hashSync(req.body.password, 10);
-        users.push(newUser);
-        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
-        res.redirect('/');
+           };        
+    },
+    login: (req, res) => {
+        res.render('login');
     },
     log: (req,res) => {
         //Logica para loguear usuario
         res.redirect('/');
     },
+    profile: (req,res) => {
+        res.render('profile', {})
+    },
+    edit: (req,res) => {
+        
+    },
+    update: (req,res) => {
+
+    },
+    delete: (req,res) => {
+
+    },
+    destroy: (req,res) => {
+
+    }
     //Editar usuario?
     //Destruir usuario?
 };
 
 module.exports = userController;
+
+
+
+//Controller para JSON
+// const userController = {
+//     register: (req, res) => {
+//         res.render('register');    
+//     },    
+//     create: (req,res) => {
+//         const errors = validationResult(req);
+//         if (!errors.isEmpty()) {
+//              res.render('register', { errors: errors.mapped() });
+//            };
+//         let newUser = req.body;
+//         newUser.id = (users.length + 1);
+//         newUser.image = req.file.filename;
+//         newUser.password = bcrypt.hashSync(req.body.password, 10);
+//         users.push(newUser);
+//         fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
+//         res.redirect('/');       
+//     },
+//     login: (req, res) => {
+//         res.render('login');
+//     },
+//     log: (req,res) => {
+//         //Logica para loguear usuario
+//         res.redirect('/');
+//     },
+//     //Editar usuario?
+//     //Destruir usuario?
+// };
+
+// module.exports = userController;
