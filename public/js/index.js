ddb18f7ca5297fa71d811d3924f49d22a0047321
federico_festa <federico.sfesta@gmail.com>
2022-09-25 15:08:51 -0300

    implement home slideshow

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,15 @@
-window.onscroll = () => {
-    const main = document.querySelector('main');
-    if (window.pageYOffset > 144) {
-        main.classList.add('scroll');
-    } else {
-        main.classList.remove('scroll');
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("banner");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-};
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 4000);
+}
