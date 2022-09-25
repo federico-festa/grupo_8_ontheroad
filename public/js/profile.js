5758cf897b834502fd7525ef0f759dae7734d952
federico_festa <federico.sfesta@gmail.com>
2022-09-24 23:32:28 -0300

    css edit

---
+++ public/js/profile.js
@@ -0,0 +1,23 @@
+
+const beach = document.querySelector('#beach');
+const mountain = document.querySelector('#mountain');
+const city = document.querySelector('#city');
+const main = document.querySelector('main');
+
+beach.addEventListener('click', () => {
+    main.classList.remove('mountain');
+    main.classList.remove('city');
+    main.classList.add('beach');
+});
+
+mountain.addEventListener('click', () => {
+    main.classList.remove('beach');
+    main.classList.remove('city');
+    main.classList.add('mountain');
+});
+
+city.addEventListener('click', () => {
+    main.classList.remove('mountain');
+    main.classList.remove('beach');
+    main.classList.add('city');
+});
