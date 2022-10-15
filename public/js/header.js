c50f5fde33f823e7bdcea416b9a8f623f316a99e
federico_festa <federico.sfesta@gmail.com>
2022-10-14 21:18:40 -0300

    header responsive droplist

--- public/js/header.js
+++ public/js/header.js
@@ -21,6 +21,8 @@ window.onload = () => {
     const logo = document.querySelector('img');
     const userIcon = document.querySelector('.fa-user');
     const droplist = document.querySelector('.droplist');
+    const userMobile = document.querySelector('#userMobile');
+    const mobileDropList = document.querySelector('.mobileDropList');
 
     logo.addEventListener('click', () => {
         window.location.href = '/';
@@ -29,11 +31,38 @@ window.onload = () => {
     userIcon.addEventListener('click', () => {
         droplist.classList.toggle('show');
     });
+
+    userMobile.addEventListener('click', () => {
+        mobileDropList.classList.toggle('show');
+    });
 };
+
+window.addEventListener('load', () => {
+    const guestMobile = document.querySelector('#guestMobile');
+    const mobileDropListGuest = document.querySelector('.mobileDropListGuest');
+
+    guestMobile.addEventListener('click', () => {
+        mobileDropListGuest.style.display = 'block';
+    })
+});
+
+window.addEventListener('click', (e) => {
+    const mobileDropListGuest = document.querySelector('.mobileDropListGuest');
+
+    if (!e.target.matches('.fa-bars')){
+        mobileDropListGuest.style.display = 'none';
+    };
+});
 
 window.onclick = (e) => {
     const droplist = document.querySelector('.droplist');
+    const mobileDropList = document.querySelector('.mobileDropList');
+
     if (!e.target.matches('.fa-user')){
         droplist.classList.remove('show');
+    };
+
+    if (!e.target.matches('.fa-user')){
+        mobileDropList.classList.remove('show');
     };
 };
