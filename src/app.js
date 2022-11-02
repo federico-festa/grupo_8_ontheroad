9fc632a82b78be039455e161e1a8e08b0b1262f8
federico_festa <federico.sfesta@gmail.com>
2022-11-02 01:43:44 -0300

    error view

--- src/app.js
+++ src/app.js
@@ -36,4 +36,18 @@ app.use('/user', userRoutes);
 app.use('/api/products', productsApiRoutes);
 app.use('/api/users', usersApiRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).render('error');
+    next();
+});
+app.use((req, res, next) => {
+    next(createError(404));
+});
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err:{};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
 module.exports = app;
