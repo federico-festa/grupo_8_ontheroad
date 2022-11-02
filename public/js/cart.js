e451723db68bc120f3ca3dd88107e36643ba3ab1
federico_festa <federico.sfesta@gmail.com>
2022-11-02 17:58:34 -0300

    upload products

--- public/js/cart.js
+++ public/js/cart.js
@@ -13,7 +13,7 @@ window.addEventListener('load', () => {
             let cart = JSON.parse(localStorage.getItem('products'));
 
             productsList.forEach((product) => {
-                if (product.id == cart[0] || cart[1] || cart[2]) {
+                if (product.id == cart[0]) {
                     const cartProduct = document.createElement('div');
                     const exp = document.createElement('div');
                     const h4 = document.createElement('h4');
@@ -38,11 +38,61 @@ window.addEventListener('load', () => {
                     priceDiv.appendChild(price);
 
                     productsCart.appendChild(cartProduct);
-                };
+                } else if (product.id == cart[1]) {
+                    const cartProduct = document.createElement('div');
+                    const exp = document.createElement('div');
+                    const h4 = document.createElement('h4');
+                    const desc = document.createElement('div');
+                    const priceDiv = document.createElement('div');
+                    const price = document.createElement('h3');
+
+                    cartProduct.setAttribute('class', 'cart-product');
+                    exp.setAttribute('class', 'exp');
+                    h4.setAttribute('class', 'name');
+                    h4.textContent = product.name;
+                    desc.setAttribute('class', 'desc');
+                    desc.textContent = product.shortDescription;
+                    priceDiv.setAttribute('class', 'price');
+                    price.setAttribute('class', 'price');
+                    price.textContent = '$' + product.price;
+
+                    cartProduct.appendChild(exp);
+                    exp.appendChild(h4);
+                    exp.appendChild(desc);
+                    cartProduct.appendChild(priceDiv);
+                    priceDiv.appendChild(price);
+
+                    productsCart.appendChild(cartProduct);
+                } else if (product.id == cart[2]) {
+                    const cartProduct = document.createElement('div');
+                    const exp = document.createElement('div');
+                    const h4 = document.createElement('h4');
+                    const desc = document.createElement('div');
+                    const priceDiv = document.createElement('div');
+                    const price = document.createElement('h3');
+
+                    cartProduct.setAttribute('class', 'cart-product');
+                    exp.setAttribute('class', 'exp');
+                    h4.setAttribute('class', 'name');
+                    h4.textContent = product.name;
+                    desc.setAttribute('class', 'desc');
+                    desc.textContent = product.shortDescription;
+                    priceDiv.setAttribute('class', 'price');
+                    price.setAttribute('class', 'price');
+                    price.textContent = '$' + product.price;
+
+                    cartProduct.appendChild(exp);
+                    exp.appendChild(h4);
+                    exp.appendChild(desc);
+                    cartProduct.appendChild(priceDiv);
+                    priceDiv.appendChild(price);
+
+                    productsCart.appendChild(cartProduct);
+                }
             });
         })
         .catch(e => console.log(e));
-        
+
     if (!localStorage.getItem('products')) {
         empty.style.display = 'block';
         checkout.style.display = 'none';
@@ -50,7 +100,7 @@ window.addEventListener('load', () => {
 
     checkoutButton.addEventListener('click', (e) => {
         localStorage.removeItem('products');
-        alert('¡Tu compra fue aprobada!');
+        alert('Serás redirigido a la plataforma de pago. ¡Gracias!');
         location.reload();
     })
 
--- public/js/profile.js
+++ public/js/profile.js
@@ -7,16 +7,36 @@ beach.addEventListener('click', () => {
     main.classList.remove('mountain');
     main.classList.remove('city');
     main.classList.add('beach');
+    localStorage.setItem('back', 'beach');
 })
 
 mountain.addEventListener('click', () => {
     main.classList.remove('beach');
     main.classList.remove('city');
     main.classList.add('mountain');
+    localStorage.setItem('back', 'mountain');
 })
 
 city.addEventListener('click', () => {
     main.classList.remove('mountain');
     main.classList.remove('beach');
     main.classList.add('city');
+    localStorage.setItem('back', 'city');
 })
+
+window.addEventListener('load', () => {
+    if (localStorage.getItem('back') == 'beach') {
+        main.classList.remove('mountain');
+        main.classList.remove('city');
+        main.classList.add('beach');
+    } else if (localStorage.getItem('back') == 'mountain') {
+        main.classList.remove('beach');
+        main.classList.remove('city');
+        main.classList.add('mountain');
+    } else if (localStorage.getItem('back') == 'city') {
+        main.classList.remove('mountain');
+        main.classList.remove('beach');
+        main.classList.add('city');
+    }
+})
+
